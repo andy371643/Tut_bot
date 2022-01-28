84900e6abd6e2553fcf8a53cee42b5055dd2c66a
andy371643 <98539835+andy371643@users.noreply.github.com>
2022-01-28 16:28:08 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,4 +7,14 @@ bot = commands.Bot(command_prefix='[')
 async def on_ready():
     print(">>古代靈異雙頭戰象已被召喚<<")
 
-bot.run('OTM2MjcyMTk1NDkwMDU0MTk0.YfKxbg.452xfOZNGrPgatu5bhLQ505H5YY')
+@bot.event
+async def on_member_join(member):
+    channel = bot.get_channel(936520716390653992)
+    await channel.send(f'{member} 滑進垃圾車')
+
+@bot.event
+async def on_member_remove(member):
+    channel = bot.get_channel(936520716390653992)
+    await channel.send(f'{member} 高歌離席')
+
+bot.run('OTM2MjcyMTk1NDkwMDU0MTk0.YfKxbg.mCZj7Bg-8VhhSpt7Y-K9630gyoA')
